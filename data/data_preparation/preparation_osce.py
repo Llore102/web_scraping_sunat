3fcbe42cc5d8025556510485beb0240f7f4c2582
LUIS\llore <lloreda102@gmail.com>
2024-03-20 02:22:51 -0500

    Data Preparation OSCE

---
+++ data/data_preparation/preparation_osce.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+directorio = '../osce'
+
+ruc_unicos = []
+
+for archivo in os.listdir(directorio):
+    if archivo.endswith('.xlsx') or archivo.endswith('.xls'):
+        df = pd.read_excel(os.path.join(directorio, archivo))
+        ruc_unicos.extend(df['RUC_CODIGO_POSTOR'].unique())
+
+df_ruc_unicos = pd.DataFrame({'RUC': list(set(ruc_unicos))})
+
+df_ruc_unicos.to_excel('../data_preparation/ruc_unicos.xlsx', index=False)
